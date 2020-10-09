ce1da66b022fa15bd98a71a83e3d1ee65ecca5ad
Sam Clercky <sam.clercky@gmail.com>
2020-10-09 13:09:43 +0200

    ceasar encryptie final ready

--- 05_davinci/final/index.js
+++ 05_davinci/final/index.js
@@ -1,14 +1,46 @@
 const resultaat = document.getElementById("resultaat");
 const code = document.getElementById("code");
+const deresultaat = document.getElementById("deresultaat");
+const decode = document.getElementById("decode");
+const bruteResult = document.getElementById("brute");
 
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz ?.:!";
-const sleutel = 5;
+const SLEUTEL = 5;
 
-let result = "";
-for (let letter of code.value) {
-	const index = letters.indexOf(letter);
-	const index2 = (index + sleutel) % letters.length;
-	result += letters[index2];
+function encrypt(sleutel) {
+	let result = "";
+	for (let letter of code.value) {
+		const index = letters.indexOf(letter);
+		const index2 = (index + sleutel) % letters.length;
+		result += letters[index2];
+	}
+
+	resultaat.innerHTML = result; 
 }
 
-resultaat.innerHTML = result; 
+encrypt(SLEUTEL);
+
+function decrypt(sleutel) {
+	let result = "";
+	for (let letter of decode.value) {
+		const index = letters.indexOf(letter);
+		let index2 = index - sleutel
+		if (index2 < 0) { // zorg er voor dat index2 niet kleiner is dan 0
+			index2 += letters.length;
+		}
+		result += letters[index2];
+	}
+	deresultaat.innerHTML = result;
+
+	return result; // geef resultaat terug
+}
+
+decrypt(SLEUTEL);
+
+function brute() {
+	bruteResult.innerHTML = ""; // maak leeg
+	for (let sleutel in letters) {
+		bruteResult.innerHTML += decrypt(sleutel) + "<br>";
+	}
+}
+brute();
