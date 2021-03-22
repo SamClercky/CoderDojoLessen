3d95d25d241f4a783a112da3611d97db45196e62
samclercky <sam.clercky@gmail.com>
2021-03-22 12:00:51 +0100

    Werkende versie transpositie

--- 06_transpositie/final/index.js
+++ 06_transpositie/final/index.js
@@ -1,70 +1,69 @@
-const resultaat = document.getElementById("resultaat");
-const code = document.getElementById("code");
-const deresultaat = document.getElementById("deresultaat");
-const decode = document.getElementById("decode");
-const bruteResult = document.getElementById("brute");
+// Dit is een optioneel deel maar vergemakkelijkt later de code
+// De variabelen verwijzen naar de verschillende elementen in de HTML code
+let resultaat = document.getElementById("resultaat");
+let code = document.getElementById("code");
+let deresultaat = document.getElementById("deresultaat");
+let decode = document.getElementById("decode");
+let bruteResult = document.getElementById("brute");
 
-const SLEUTEL = 3;
+// De sleutel van het bericht moet normaal geheim worden gehouden (wachtwoord)
+let SLEUTEL = 3;
 
-function encrypt(sleutel) {
+// Versleutelt het bericht dat wordt meegegeven via een transpositie
+// abcd ->  a | b | c | -> a d b _ c _
+//          d | _ | _ |
+function encrypt(sleutel, bericht) {
 	let result = "";
 
-	const bericht = code.value;
-	const aantalKolommen = sleutel;
-	const aantalRijen = Math.ceil(bericht.length / aantalKolommen);
+	let aantalKolommen = sleutel;
+	let aantalRijen = Math.ceil(bericht.length / aantalKolommen);
 
 	for (let kolom = 0; kolom < aantalKolommen; kolom++) {
 		for (let rij = 0; rij < aantalRijen; rij++) {
-			const positie = kolom + rij*aantalKolommen;
-			result += bericht[positie] || "";
+			let positie = kolom + rij*aantalKolommen;
+			result += bericht[positie] || " ";
 		}
 	}
-
-	resultaat.innerHTML = result;
-}
-
-encrypt(SLEUTEL);
-
-function decrypt(sleutel) {
-	let result = "";
-
-	// merk op dat rijen en kolommen verwisseld zijn
-	const bericht = decode.value;
-	const aantalRijen = sleutel;
-	const aantalKolommen = Math.ceil(bericht.length / aantalRijen);
-
-	// Soms hebben we te veel doosje ==> vervangen door '_'
-	// We veranderen het bericht zodat deze altijd aantalRijen*aantalKolommen tekens heeft
-	const teVeelDoosjes = aantalRijen*aantalKolommen - bericht.length;
-	let nogTeVindenDoosjes = teVeelDoosjes;
-
-	for (let kolom = 0; kolom < aantalKolommen; kolom++) {
-		for (let rij = 0; rij < aantalRijen; rij++) {
-			if (kolom >= aantalKolommen - 2 && rij >= aantalRijen-nogTeVindenDoosjes) {
-				nogTeVindenDoosjes--;
-				continue;
-			} else {
-				const positie = kolom + rij*aantalKolommen;
-				const voorbijeDoosjes = rij-(aantalRijen-teVeelDoosjes);
-				result += bericht[positie] || "";
-			}
-		}
-	}
-
-	deresultaat.innerHTML = result;
 
 	return result;
 }
 
-decrypt(SLEUTEL);
+// Functie die wordt aangeroepen vanuit de HTML en toont het resultaat op het 
+// scherm
+function encryptBericht() {
+	let bericht = code.value;
 
+	resultaat.innerHTML = encrypt(SLEUTEL, bericht);
+}
+
+// Decryptie is hetzelfde als encryptie maar met een andere sleutel:
+// Nieuwe sleutel: lengte bericht / originele sleutel
+// a d b _ c _ ->  a | d | -> a b c d _ _
+//                 b | _ |
+//                 c | _ |
+function decrypt(sleutel, bericht) {
+	let nieuweSleutel = bericht.length / sleutel;
+
+	let result = encrypt(nieuweSleutel, bericht);
+
+	return result;
+}
+
+// Functie die wordt aangeroepen vanuit de HTML en toont het resultaat op het 
+// scherm
+function decryptBericht() {
+	let bericht = decode.value;
+
+	deresultaat.innerHTML = decrypt(SLEUTEL, bericht);
+}
+
+// Breken van de code: Snel uitproberen van alle mogelijkheden en het resultaat
+// zo vinden.
 function brute() {
-	bruteResult.innerHTML = ""; // maak leeg
-
 	const aantalMogelijkheden = decode.value.length;
+	let bericht = decode.value;
 
 	for (let sleutel = 1; sleutel < aantalMogelijkheden; sleutel++) {
-		bruteResult.innerHTML += decrypt(sleutel) + "<br>";
+		bruteResult.innerHTML += decrypt(sleutel, bericht) + "<br>";
 	}
 }
-//brute();
